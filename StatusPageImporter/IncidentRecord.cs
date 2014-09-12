bfb0dac53279d6799d0ea7454936ac52532e3d48
fandrei <andrei.faber@gmail.com>
2014-09-12 11:17:08 +0700

    write data to JSON log file

---
+++ StatusPageImporter/IncidentRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatusPageImporter
+{
+	class IncidentRecord
+	{
+		public string Message;
+		public string Type = "StatusPage";
+	}
+}
--- StatusPageImporter/Program.cs
+++ StatusPageImporter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -18,10 +19,31 @@ namespace StatusPageImporter
 				var baseUrl = AppSettings.BaseUrl;
 				var response = HttpUtil.Request(baseUrl + "incidents.json?api_key=" + AppSettings.ApiKey);
 				var data = (JArray)JsonConvert.DeserializeObject(response);
+
+				var dataPath = @"..\_data";
+				if (!Directory.Exists(dataPath))
+					Directory.CreateDirectory(dataPath);
+
+				foreach (var file in Directory.GetFiles(dataPath))
+				{
+					File.Delete(file);
+				}
+
+				var records = new List<IncidentRecord>();
 				foreach (var item in data)
 				{
-					
+					var record = new IncidentRecord { Message = item.ToString() };
+					records.Add(record);
+
+					var text = item.ToString();
+					text = text.Replace("\r", "\\r").Replace("\n", "\\n").Replace("\"", "\\\"");
+
+					var message = string.Format(
+						"{{\"timestamp\":\"{0}\",\"message\":\"{1}\",\"source\":\"StatusPage\"}}\r\n",
+						DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"), text);
+					File.AppendAllText(dataPath + "\\log.txt", message);
 				}
+
 			}
 			catch (Exception exc)
 			{
